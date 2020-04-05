1c943b643366a5215e45e18a302b0ba93ba40caf
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-04-05 10:16:54 +0100

    Updated scroll tolerance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,30 @@
 // Hide Header on on scroll down
 var didScroll;
 var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = document.querySelector('.header').clientHeight;
+
+var className = {
+    scrolled: 'is-scrolled',
+    downwards: 'is-scrolled--downwards',
+    upwards: 'is-scrolled--upwards'
+};
 
 window.addEventListener('scroll', function() {
     var scrollPosition = window.pageYOffset;
+    var navbarHeight = document.querySelector('.header').clientHeight;
+    var tolerance = navbarHeight;
 
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - scrollPosition) <= delta)
+    // Make sure they scroll more than tolerance
+    if(Math.abs(lastScrollTop - scrollPosition) <= tolerance)
         return;
 
     if(scrollPosition < navbarHeight) {
-        document.body.classList.remove('is-scrolled', 'is-scrolled--upwards', 'is-scrolled--downwards');
+        document.body.classList.remove(className.scrolled, className.upwards, className.downwards);
     } else if(scrollPosition < lastScrollTop) {
-        document.body.classList.add('is-scrolled', 'is-scrolled--upwards');
-        document.body.classList.remove('is-scrolled--downwards');
+        document.body.classList.add(className.scrolled, className.upwards);
+        document.body.classList.remove(className.downwards);
     } else {
-        document.body.classList.add('is-scrolled', 'is-scrolled--downwards');
-        document.body.classList.remove('is-scrolled--upwards');
+        document.body.classList.add(className.scrolled, className.downwards);
+        document.body.classList.remove(className.upwards);
     }
 
     lastScrollTop = scrollPosition;
