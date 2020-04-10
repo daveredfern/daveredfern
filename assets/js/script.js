fece2ea64d79dd1266048ed5f5044c0da928edde
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-04-10 14:40:38 +0100

    Updating darkmode.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 /* Hide Header on on scroll down */
-// Hide Header on on scroll down
 var didScroll;
 var lastScrollTop = 0;
 
@@ -9,18 +8,18 @@ var className = {
     upwards: 'is-scrolled--upwards'
 };
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var scrollPosition = window.pageYOffset;
     var navbarHeight = document.querySelector('.header').clientHeight;
     var tolerance = navbarHeight / 2;
 
     // Make sure they scroll more than tolerance
-    if(Math.abs(lastScrollTop - scrollPosition) <= tolerance)
+    if (Math.abs(lastScrollTop - scrollPosition) <= tolerance)
         return;
 
-    if(scrollPosition < tolerance) {
+    if (scrollPosition < tolerance) {
         document.body.classList.remove(className.scrolled, className.upwards, className.downwards);
-    } else if(scrollPosition < lastScrollTop) {
+    } else if (scrollPosition < lastScrollTop) {
         document.body.classList.add(className.scrolled, className.upwards);
         document.body.classList.remove(className.downwards);
     } else {
@@ -33,6 +32,6 @@ window.addEventListener('scroll', function() {
 
 /* Form forms, auto fill in the referrer URL */
 var referrer = document.getElementById('referrer');
-if(referrer) {
+if (referrer) {
     referrer.value = document.referrer;
 }
