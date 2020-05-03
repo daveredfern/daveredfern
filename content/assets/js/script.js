3b10c4bc9317ccad542ccb01eac302cbf09f29b7
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-05-03 08:09:52 +0100

    Fixing lighthouse issues

--- content/assets/js/script.js
+++ content/assets/js/script.js
@@ -1,4 +1,5 @@
 /* Add arrow to external links and making them open in a new window */
+
 document.querySelectorAll('a').forEach(function (link) {
     if (
         link.href.indexOf('daveredfern.com') == -1 &&
@@ -8,10 +9,13 @@ document.querySelectorAll('a').forEach(function (link) {
     ) {
         link.append(' ↗');
         link.setAttribute('target', '_blank');
+        link.setAttribute('rel', 'noopener noreferrer');
+
     }
 });
 
 /* Add hash to h2 */
+
 document.querySelectorAll('[role="main"] h2, [role="main"] h3').forEach(function (link) {
     var hash = link.textContent.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
     var anchor = document.createElement('a');
