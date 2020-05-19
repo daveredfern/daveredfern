90556b0e9f897e3d467abbd86c1d34efcfa4d95a
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-05-19 17:03:30 +0100

    Update sw.js

--- content/assets/js/sw.js
+++ content/assets/js/sw.js
@@ -1,5 +1,5 @@
 var cacheName = 'csswizardry:0124';
-var cacheFiles = ['/', '/about/', '/blog/', '/contact/', '/now/', '/privacy/', '/style-guide/', '/vegan/', '//hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/', '/getting-sass-using-arrays-functions-loops/', '/screen-sizes/', '/green-smoothie/', '/always-leave-things-cleaner-than-you-found-them/', '/enabling-a-chance-for-something-to-happen/', '/design-principles/' ];
+var cacheFiles = ['/', '/about/', '/blog/', '/contact/', '/now/', '/privacy/', '/style-guide/', '/vegan/', '/hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/', '/getting-sass-using-arrays-functions-loops/', '/screen-sizes/', '/green-smoothie/', '/always-leave-things-cleaner-than-you-found-them/', '/enabling-a-chance-for-something-to-happen/', '/design-principles/' ];
 self.addEventListener('install', function (event) {
     event.waitUntil(caches.open(cacheName).then(function (cache) {
         return cache.addAll(cacheFiles);
