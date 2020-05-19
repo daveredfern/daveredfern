d8317a140ef3576b57d4093dbb278620a7c0f462
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-05-19 17:01:23 +0100

    Adding offline content.

---
+++ content/assets/js/sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'csswizardry:0124';
+var cacheFiles = ['/', '/about/', '/blog/', '/contact/', '/now/', '/privacy/', '/style-guide/', '/vegan/', '//hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/', '/getting-sass-using-arrays-functions-loops/', '/screen-sizes/', '/green-smoothie/', '/always-leave-things-cleaner-than-you-found-them/', '/enabling-a-chance-for-something-to-happen/', '/design-principles/' ];
+self.addEventListener('install', function (event) {
+    event.waitUntil(caches.open(cacheName).then(function (cache) {
+        return cache.addAll(cacheFiles);
+    }));
+});
+self.addEventListener('fetch', function (event) {
+    event.respondWith(caches.match(event.request).then(function (response) {
+        if (response) {
+            return response;
+        }
+        return fetch(event.request);
+    }).catch(function () {
+        return caches.match('/offline/');
+    }));
+});
+self.addEventListener('activate', function (event) {
+    var cacheWhitelist = ['csswizardry:0124'];
+    event.waitUntil(caches.keys().then(function (cacheNames) {
+        return Promise.all(cacheNames.map(function (cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+            }
+        }));
+    }));
+});
---
+++ content/offline.md
@@ -0,0 +1,7 @@
+---
+title: "It looks like you’re offline!"
+---
+
+It looks like your device has lost it’s internet connection. While we wait for your connection to come back, I’ve prepare some offline content for your to read.
+
+If you visit a page and it redirects you back to this one, it means it’s not available offline.
