d86e2d7fc40ca8d5d933e0579170bc2985a53480
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-05-19 17:12:13 +0100

    Update sw.js

--- content/assets/js/sw.js
+++ content/assets/js/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0124';
+var cacheName = 'daveredfern:0124';
 var cacheFiles = ['/', '/about/', '/blog/', '/contact/', '/now/', '/privacy/', '/style-guide/', '/vegan/', '/hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/', '/getting-sass-using-arrays-functions-loops/', '/screen-sizes/', '/green-smoothie/', '/always-leave-things-cleaner-than-you-found-them/', '/enabling-a-chance-for-something-to-happen/', '/design-principles/' ];
 self.addEventListener('install', function (event) {
     event.waitUntil(caches.open(cacheName).then(function (cache) {
@@ -16,7 +16,7 @@ self.addEventListener('fetch', function (event) {
     }));
 });
 self.addEventListener('activate', function (event) {
-    var cacheWhitelist = ['csswizardry:0124'];
+    var cacheWhitelist = ['daveredfern:0124'];
     event.waitUntil(caches.keys().then(function (cacheNames) {
         return Promise.all(cacheNames.map(function (cacheName) {
             if (cacheWhitelist.indexOf(cacheName) === -1) {
