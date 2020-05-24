d93d1454283f943c8b46890da1306d620ccade70
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-05-24 15:02:48 +0100

    Sorting out sw stuff

--- content/sw.js
+++ content/sw.js
@@ -1,31 +1,46 @@
-var cacheName = 'daveredfern:0125';
+const staticCacheName = 'daveredfern-v1';
+const assets = [
+    '/',
+    '/about/',
+    '/blog/',
+    '/contact/',
+    '/now/',
+    '/hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/',
+    '/getting-sass-using-arrays-functions-loops/',
+    '/screen-sizes/',
+    '/green-smoothie/',
+    '/always-leave-things-cleaner-than-you-found-them/',
+    '/enabling-a-chance-for-something-to-happen/',
+    '/design-principles/',
+];
 
-var cacheFiles = ['/', '/about/', '/blog/', '/contact/', '/now/', '/hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/', '/getting-sass-using-arrays-functions-loops/', '/screen-sizes/', '/green-smoothie/', '/always-leave-things-cleaner-than-you-found-them/', '/enabling-a-chance-for-something-to-happen/', '/design-principles/', ];
-
-self.addEventListener('install', function (event) {
-    event.waitUntil(caches.open(cacheName).then(function (cache) {
-        return cache.addAll(cacheFiles);
-    }));
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(caches.match(event.request).then(function (response) {
-        if (response) {
-            return response;
-        }
-        return fetch(event.request);
-    }).catch(function () {
-        return caches.match('/offline/');
-    }));
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
-self.addEventListener('activate', function (event) {
-    var cacheWhitelist = ['daveredfern:0125'];
-    event.waitUntil(caches.keys().then(function (cacheNames) {
-        return Promise.all(cacheNames.map(function (cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-                return caches.delete(cacheName);
-            }
-        }));
-    }));
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
