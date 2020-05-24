01a87fef383f52533b8feb3b7b24d4e4be027499
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-05-24 09:51:49 +0100

    Sorting our service worker

--- content/assets/js/sw.js
+++ content/assets/js/sw.js
@@ -1,6 +1,6 @@
 var cacheName = 'daveredfern:0125';
 
-var cacheFiles = ['/', '/about/', '/blog/', '/contact/', '/now/', '/hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/', '/getting-sass-using-arrays-functions-loops/', '/screen-sizes/', '/green-smoothie/', '/always-leave-things-cleaner-than-you-found-them/', '/enabling-a-chance-for-something-to-happen/', '/design-principles/'];
+var cacheFiles = ['/', '/about/', '/blog/', '/contact/', '/now/', '/hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/', '/getting-sass-using-arrays-functions-loops/', '/screen-sizes/', '/green-smoothie/', '/always-leave-things-cleaner-than-you-found-them/', '/enabling-a-chance-for-something-to-happen/', '/design-principles/', ];
 
 self.addEventListener('install', function (event) {
     event.waitUntil(caches.open(cacheName).then(function (cache) {
