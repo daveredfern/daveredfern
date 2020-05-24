12a781ea7740c07d07752a639fdf047df3c75041
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-05-24 09:30:29 +0100

    Making a progressive web app

--- content/assets/js/sw.js
+++ content/assets/js/sw.js
@@ -1,10 +1,13 @@
-var cacheName = 'daveredfern:0124';
-var cacheFiles = ['/', '/about/', '/blog/', '/contact/', '/now/', '/privacy/', '/style-guide/', '/vegan/', '/hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/', '/getting-sass-using-arrays-functions-loops/', '/screen-sizes/', '/green-smoothie/', '/always-leave-things-cleaner-than-you-found-them/', '/enabling-a-chance-for-something-to-happen/', '/design-principles/' ];
+var cacheName = 'daveredfern:0125';
+
+var cacheFiles = ['/', '/about/', '/blog/', '/contact/', '/now/', '/hide-or-exclude-folders-and-files-from-search-in-visual-studio-code/', '/getting-sass-using-arrays-functions-loops/', '/screen-sizes/', '/green-smoothie/', '/always-leave-things-cleaner-than-you-found-them/', '/enabling-a-chance-for-something-to-happen/', '/design-principles/'];
+
 self.addEventListener('install', function (event) {
     event.waitUntil(caches.open(cacheName).then(function (cache) {
         return cache.addAll(cacheFiles);
     }));
 });
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(caches.match(event.request).then(function (response) {
         if (response) {
@@ -15,8 +18,9 @@ self.addEventListener('fetch', function (event) {
         return caches.match('/offline/');
     }));
 });
+
 self.addEventListener('activate', function (event) {
-    var cacheWhitelist = ['daveredfern:0124'];
+    var cacheWhitelist = ['daveredfern:0125'];
     event.waitUntil(caches.keys().then(function (cacheNames) {
         return Promise.all(cacheNames.map(function (cacheName) {
             if (cacheWhitelist.indexOf(cacheName) === -1) {
