c68c0c18f970691b13fd1308cba316c5953e0169
Dave Redfern <dave.redfern@conversion-rate-experts.com>
2020-05-25 09:35:55 +0100

    Update sw.js

--- content/sw.js
+++ content/sw.js
@@ -34,7 +34,7 @@ self.addEventListener('fetch', function (event) {
 });
 
 self.addEventListener('activate', function (event) {
-    var cacheWhitelist = [cacheName];
+    var cacheWhitelist = ["daveredfern-v3"];
     event.waitUntil(caches.keys().then(function (cacheNames) {
         return Promise.all(cacheNames.map(function (cacheName) {
             if (cacheWhitelist.indexOf(cacheName) === -1) {
